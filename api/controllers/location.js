3a3bf4215ed03ee5aeacb2f4e035e235cf9e4653
collins njau <collinsnjau39@gmail.com>
2019-08-14 13:10:03 +0300

    Finish the CRUD functionality - add get single location functionality - add edit single location functionality - add delete single location functionality - fix naming of routes

--- api/controllers/location.js
+++ api/controllers/location.js
@@ -52,3 +52,59 @@ exports.location_create = (req, res, next) => {
       });
     });
 };
+
+exports.location_get_one = (req, res, next) => {
+  const { locationId } = req.params;
+  Location.findById(locationId)
+    .then(location => {
+      if (location) {
+        const response = {
+          name: location.name,
+          femalePopulation: location.femalePopulation,
+          malePopulation: location.malePopulation,
+          totalPopulation: location.totalPopulation,
+          _id: location._id
+        };
+        res.status(200).json(response);
+      } else {
+        res.status(404).json({
+          message: "Location does not exist"
+        });
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ error: error });
+    });
+};
+
+exports.location_patch_one = (req, res, next) => {
+  const { locationId } = req.params;
+  const updateOps = {};
+  for (const ops of req.body) {
+    updateOps[ops.propsName] = ops.value;
+  }
+  Location.update({ _id: locationId }, { $set: updateOps })
+    .exec()
+    .then(result => {
+      res.status(200).json(result);
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: err
+      });
+    });
+};
+
+exports.location_delete_one = (req, res, next) => {
+  const { locationId } = req.params;
+  Location.remove({ _id: locationId })
+    .then(() => {
+      res.status(200).json({
+        message: "Successfully deleted the location"
+      });
+    })
+    .catch(err => {
+      res.status(500).json({ error: err });
+    });
+};
--- api/routes/location.js
+++ api/routes/location.js
@@ -4,5 +4,10 @@ const LocationController = require("../controllers/location");
 
 router.get("/", LocationController.location_get_all);
 router.post("/", LocationController.location_create);
+router.get("/:locationId", LocationController.location_get_one);
+
+router.patch("/:locationId", LocationController.location_patch_one);
+
+router.delete("/:locationId", LocationController.location_delete_one);
 
 module.exports = router;
